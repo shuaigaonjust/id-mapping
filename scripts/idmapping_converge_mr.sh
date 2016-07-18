64a08b856a60d98d0e16d033dc8639288f6dc18a
jiezhang13 <jiezhang13@iflytek.com>
2016-07-18 18:08:36 +0800

    sh-new

--- scripts/idmapping_converge_mr.sh
+++ scripts/idmapping_converge_mr.sh
@@ -4,19 +4,22 @@ rm -f cmd.txt
 
 DAY=$1
 INPUT_PATH="/user/compass/public/hive/idmapping/ids/product=original/day=$DAY"
-OUTPUT_PATH_PREFIX="/user/compass/public/hive/idmapping/data/idmapping_converge/MR"
+OUTPUT_PATH_PREFIX="/user/compass/public/hive/idmapping/ids_2"
 
 for PRODUCT in imei mac idfa openudid phonenumber imsi; do
   for step in 1 2 3; do 
     echo "**********  start run ${PRODUCT} step ${step}  **********\n"
     
     hadoop fs -rm -r $OUTPUT_PATH_PREFIX"/product="${PRODUCT}"_step_"${step}
-    
-    echo "hadoop jar IDMapping-1.0-SNAPSHOT-jar-with-dependencies.jar com.iflytek.hadoop.idmapping.main.IdMappingMain${step}  -Dmapred.reduce.tasks=110 -Dmapreduce.task.timeout=3600000  -Dmapreduce.map.memory.mb=9216 -Dmapreduce.reduce.memory.mb=9216 -Dmapreduce.map.java.opts=-Xmx9216m -Dmapreduce.reduce.java.opts=-Xmx9216m  -Dmapred.job.shuffle.merge.percent=0.6  -Dmapreduce.job.queuename=dmp  $INPUT_PATH  $OUTPUT_PATH_PREFIX  ${PRODUCT}_step_${step} ${DAY} ${PRODUCT}" >> cmd.txt
-    
-    hadoop jar IDMapping-1.0-SNAPSHOT-jar-with-dependencies.jar com.iflytek.hadoop.idmapping.main.IdMappingMain${step}  -Dmapred.reduce.tasks=110 -Dmapreduce.task.timeout=3600000  -Dmapreduce.map.memory.mb=9216 -Dmapreduce.reduce.memory.mb=9216 -Dmapreduce.map.java.opts=-Xmx9216m -Dmapreduce.reduce.java.opts=-Xmx9216m  -Dmapred.job.shuffle.merge.percent=0.6  -Dmapreduce.job.queuename=dmp  $INPUT_PATH  $OUTPUT_PATH_PREFIX ${PRODUCT}_step_${step} ${DAY} ${PRODUCT}
-    
-    
+   
+    if [ ${PRODUCT} = imsi ] && [ ${step} = 3 ];then
+           echo "hadoop jar IDMapping-1.0-SNAPSHOT-jar-with-dependencies.jar com.iflytek.hadoop.idmapping.main.IdMappingMain${step}  -Dmapred.reduce.tasks=110 -Dmapreduce.task.timeout=3600000  -Dmapreduce.map.memory.mb=9216 -Dmapreduce.reduce.memory.mb=9216 -Dmapreduce.map.java.opts=-Xmx9216m -Dmapreduce.reduce.java.opts=-Xmx9216m  -Dmapred.job.shuffle.merge.percent=0.6  -Dmapreduce.job.queuename=dmp  $INPUT_PATH  $OUTPUT_PATH_PREFIX  release ${DAY} ${PRODUCT}" >> cmd.txt
+           hadoop jar IDMapping-1.0-SNAPSHOT-jar-with-dependencies.jar com.iflytek.hadoop.idmapping.main.IdMappingMain${step}  -Dmapred.reduce.tasks=110 -Dmapreduce.task.timeout=3600000  -Dmapreduce.map.memory.mb=9216 -Dmapreduce.reduce.memory.mb=9216 -Dmapreduce.map.java.opts=-Xmx9216m -Dmapreduce.reduce.java.opts=-Xmx9216m  -Dmapred.job.shuffle.merge.percent=0.6  -Dmapreduce.job.queuename=dmp  $INPUT_PATH  $OUTPUT_PATH_PREFIX release ${DAY} ${PRODUCT}
+    else
+           echo "hadoop jar IDMapping-1.0-SNAPSHOT-jar-with-dependencies.jar com.iflytek.hadoop.idmapping.main.IdMappingMain${step}  -Dmapred.reduce.tasks=110 -Dmapreduce.task.timeout=3600000  -Dmapreduce.map.memory.mb=9216 -Dmapreduce.reduce.memory.mb=9216 -Dmapreduce.map.java.opts=-Xmx9216m -Dmapreduce.reduce.java.opts=-Xmx9216m  -Dmapred.job.shuffle.merge.percent=0.6  -Dmapreduce.job.queuename=dmp  $INPUT_PATH  $OUTPUT_PATH_PREFIX  ${PRODUCT}_step_${step} ${DAY} ${PRODUCT}" >> cmd.txt
+           hadoop jar IDMapping-1.0-SNAPSHOT-jar-with-dependencies.jar com.iflytek.hadoop.idmapping.main.IdMappingMain${step}  -Dmapred.reduce.tasks=110 -Dmapreduce.task.timeout=3600000  -Dmapreduce.map.memory.mb=9216 -Dmapreduce.reduce.memory.mb=9216 -Dmapreduce.map.java.opts=-Xmx9216m -Dmapreduce.reduce.java.opts=-Xmx9216m  -Dmapred.job.shuffle.merge.percent=0.6  -Dmapreduce.job.queuename=dmp  $INPUT_PATH  $OUTPUT_PATH_PREFIX ${PRODUCT}_step_${step} ${DAY} ${PRODUCT}
+    fi
+
     if [ $? -eq 0 ];then
       echo "\n**********  ${PRODUCT} step ${step} success  **********\n"
     else
@@ -28,3 +31,12 @@ for PRODUCT in imei mac idfa openudid phonenumber imsi; do
   done
 done
 
+for PRODUCT in imei mac idfa openudid phonenumber imsi; do
+  for step in 1 2 3; do 
+    if [ ${PRODUCT} = imsi ] && [ ${step} = 3 ];then
+      echo "not remove release"
+    else
+      hadoop fs -rm -r $OUTPUT_PATH_PREFIX"/product="${PRODUCT}"_step_"${step}
+    fi
+  done
+ done
--- scripts/idmapping_converge_spark.sh
+++ scripts/idmapping_converge_spark.sh
@@ -4,18 +4,22 @@ rm -f cmd.txt
 
 DAY=$1
 INPUT_PATH="hdfs://ns-hf/user/compass/public/hive/idmapping/ids/product=original/day=$DAY"
-OUTPUT_PATH_PREFIX='hdfs://ns-hf/user/compass/public/hive/idmapping/data/idmapping_converge/spark'
+OUTPUT_PATH_PREFIX="hdfs://ns-hf/user/compass/public/hive/idmapping/ids_2"
 
 for PRODUCT in imei mac idfa openudid phonenumber imsi; do
   for step in 1 2 3; do 
     echo "**********  start run ${PRODUCT} step ${step}  **********\n"
        
     hadoop fs -rm -r $OUTPUT_PATH_PREFIX"/product="${PRODUCT}"_step_"${step}
+  
+    if [ ${PRODUCT} = imsi ] && [ ${step} = 3 ];then
+           echo "spark-submit --class idmapping.SparkIdMapping${step}  --master yarn-cluster --num-executors 100 --driver-memory 10g --executor-memory 15g --queue dmp ./IdMapping-1.0-SNAPSHOT-jar-with-dependencies.jar $INPUT_PATH   hdfs://ns-hf/user/compass/public/hive/idmapping/ids_2  release ${DAY} ${PRODUCT}" >> cmd.txt
+           spark-submit --class idmapping.SparkIdMapping${step}  --master yarn-cluster --num-executors 100 --driver-memory 10g --executor-memory 15g --queue dmp ./IdMapping-1.0-SNAPSHOT-jar-with-dependencies.jar $INPUT_PATH   $OUTPUT_PATH_PREFIX  release ${DAY} ${PRODUCT}
+    else
+           echo "spark-submit --class idmapping.SparkIdMapping${step}  --master yarn-cluster --num-executors 100 --driver-memory 10g --executor-memory 15g --queue dmp ./IdMapping-1.0-SNAPSHOT-jar-with-dependencies.jar $INPUT_PATH   hdfs://ns-hf/user/compass/public/hive/idmapping/ids_2  ${PRODUCT}_step_${step} ${DAY} ${PRODUCT}" >> cmd.txt
+           spark-submit --class idmapping.SparkIdMapping${step}  --master yarn-cluster --num-executors 100 --driver-memory 10g --executor-memory 15g --queue dmp ./IdMapping-1.0-SNAPSHOT-jar-with-dependencies.jar $INPUT_PATH   $OUTPUT_PATH_PREFIX  ${PRODUCT}_step_${step} ${DAY} ${PRODUCT}
+    fi
 
-    echo "spark-submit --class idmapping.SparkIdMapping${step}  --master yarn-cluster --num-executors 100 --driver-memory 2g --executor-memory 15g --queue dmp ./zhangjie-1.0-SNAPSHOT-jar-with-dependencies.jar $INPUT_PATH   hdfs://ns-hf/user/compass/public/hive/idmapping/data/idmapping_converge/spark  ${PRODUCT}_step_${step} ${DAY} ${PRODUCT}" >> cmd.txt
-    
-    spark-submit --class idmapping.SparkIdMapping${step}  --master yarn-cluster --num-executors 100 --driver-memory 2g --executor-memory 15g --queue dmp ./zhangjie-1.0-SNAPSHOT-jar-with-dependencies.jar $INPUT_PATH   $OUTPUT_PATH_PREFIX  ${PRODUCT}_step_${step} ${DAY} ${PRODUCT}
-    
     if [ $? -eq 0 ];then
       echo "\n**********  ${PRODUCT} step ${step} success  **********\n"
     else
@@ -26,3 +30,14 @@ for PRODUCT in imei mac idfa openudid phonenumber imsi; do
     INPUT_PATH=$OUTPUT_PATH_PREFIX"/product="${PRODUCT}"_step_"${step}"/day="${DAY}
   done
 done
+
+
+for PRODUCT in imei mac idfa openudid phonenumber imsi; do
+  for step in 1 2 3; do 
+    if [ ${PRODUCT} = imsi ] && [ ${step} = 3 ];then
+      echo "not remove release"
+    else
+      hadoop fs -rm -r $OUTPUT_PATH_PREFIX"/product="${PRODUCT}"_step_"${step}
+    fi
+  done
+ done
