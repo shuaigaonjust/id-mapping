e2de7a8e6ca8b91fb1108076578f4772ead65996
q313316432 <ustcchentao@qq.com>
2016-07-26 17:22:36 +0800

    add UDFIncreaseActivity.java

---
+++ idmapping_udf/src/main/java/UDFIncreaseActivity.java
@@ -0,0 +1,27 @@
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by admin on 2016/7/26.
+ */
+public class UDFIncreaseActivity extends UDF {
+    public Map<String, Integer> evaluate(Map<String, Integer> map) {
+        Map<String, Integer> mapResult = new HashMap<String, Integer>();
+        for(Map.Entry<String, Integer> tmp : map.entrySet()) {
+            if ((tmp.getValue() & 0xff) >= 254) {
+                continue;
+            }
+            mapResult.put(tmp.getKey(), tmp.getValue() + 1);
+        }
+        return mapResult;
+    }
+
+    public static void main(String[] args) {
+        UDFIncreaseActivity udfIncreaseActivity = new UDFIncreaseActivity();
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("1", 1); map.put("2", 2); map.put("3", 3); map.put("4", 4); map.put("254", 256);
+        System.out.println(udfIncreaseActivity.evaluate(map).get("254") & 0xff00);
+    }
+}
