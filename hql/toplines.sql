258ddfb5d1fbf4838a6ee4749e410e6a4ddcb4a6
JinkeCao <jcao2014@outlook.com>
2016-08-01 15:42:00 +0800

    daily update

---
+++ hql/toplines.sql
@@ -0,0 +1,9 @@
+SELECT  DISTINCT(Num) AS ConsecutiveNums
+FROM (
+    SELECT
+    Num,
+    @counter := IF(@prev = Num, @counter + 1, 1) AS how_many_cnt_in_a_row,
+    @prev := Num
+    FROM Logs y, (SELECT @counter:=1, @prev:=NULL) vars
+) sq
+WHERE how_many_cnt_in_a_row >= 3
