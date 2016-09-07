7c617cb41daa6a496ccb87b896fbb28b7e4246ae
q313316432 <ustcchentao@qq.com>
2016-09-07 14:29:30 +0800

    connect watcher

---
+++ idmapping_open_api/src/main/java/com/iflytek/cloudplatform/dmp/idmapping/ConnectWatcher.java
@@ -0,0 +1,43 @@
+package com.iflytek.cloudplatform.dmp.idmapping;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by admin on 2016/9/5.
+ */
+
+public class ConnectWatcher implements Watcher {
+    private static final int SESSION_TIMEOUT = 5000;
+    private CountDownLatch countDownLatch = new CountDownLatch(1);
+    private ZooKeeper zk;
+
+    public void connect(String hosts) throws IOException, InterruptedException {
+        zk = new ZooKeeper(hosts, SESSION_TIMEOUT, this);
+        countDownLatch.await();
+    }
+
+    public void process(WatchedEvent watchedEvent) {
+        if (watchedEvent.getState() == Event.KeeperState.SyncConnected) {
+            countDownLatch.countDown();
+        }
+    }
+
+    public void setData(String path, String data) throws KeeperException, InterruptedException {
+        zk.setData(path, data.getBytes(), -1);
+    }
+
+    public String getData(String path, Watcher watcher) throws KeeperException, InterruptedException {
+        byte[] data = zk.getData(path, watcher, null);
+        return new String(data);
+    }
+
+    public void close() throws InterruptedException {
+        zk.close();
+    }
+}
